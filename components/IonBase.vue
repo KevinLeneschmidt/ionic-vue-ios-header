d9dd76236d2dc8f1646503b00d731fd53b11c8ac
Kevin Leneschmidt <kevin@gates-app.de>
2024-04-04 19:56:11 +0200

    Initial commit

---
+++ components/IonBase.vue
@@ -0,0 +1,49 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-back-button color="secondary" text="" />
+        </ion-buttons>
+        <ion-buttons slot="end">
+          <ion-menu-button color="primary" />
+        </ion-buttons>
+        <ion-title>{{ title }}</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content :fullscreen="contentFullscreen">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">{{ title }}</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <div :class="noContentPadding ? '' : 'ion-padding'">
+        <slot />
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+<script setup lang="ts">
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonBackButton,
+  IonMenuButton,
+  IonTitle,
+  IonContent,
+} from '@ionic/vue'
+
+interface Props {
+  title: string
+  noContentPadding?: boolean
+  contentFullscreen?: boolean
+}
+
+withDefaults(defineProps<Props>(), {
+  noContentPadding: false,
+  contentFullscreen: true,
+})
+</script>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,7 @@
+<template>
+  <ion-base title="Home"> 123 </ion-base>
+</template>
+
+<script setup lang="ts">
+import IonBase from '@/components/IonBase.vue'
+</script>
